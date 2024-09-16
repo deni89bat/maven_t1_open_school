3fa4734918ea06b44be8c5085e5338a25f9e4a60
deni89bat <DPisyaev@lanit.ru>
2024-09-16 09:46:14 +0300

    init

---
+++ api-tests/src/test/java/tests/ApiTest.java
@@ -0,0 +1,50 @@
+package tests;
+
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApiTest {
+
+    static {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void getUserTest() {
+        Response response = getUser(1);
+        validateResponseStatus(response);
+        String name = extractName(response);
+        validateUserName(name);
+    }
+
+    @Step("Получение пользователя с ID {userId}")
+    public Response getUser(int userId) {
+        return RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/users/" + userId)
+                .then()
+                .extract().response();
+    }
+
+    @Step("Проверка статуса ответа")
+    public void validateResponseStatus(Response response) {
+        assertEquals(200, response.getStatusCode(), "Неверный статус ответа");
+    }
+
+    @Step("Извлечение имени пользователя из ответа")
+    public String extractName(Response response) {
+        return response.jsonPath().getString("name");
+    }
+
+    @Step("Проверка имени пользователя")
+    public void validateUserName(String name) {
+        assertEquals("Leanne Graham", name, "Ожидалось имя 'Leanne Graham', но было получено другое.");
+    }
+}
